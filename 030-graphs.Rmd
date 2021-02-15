b8e8ac04ef9e6f77a216935937192fce92e27a6f
Tom Stafford <tom@idiolect.org.uk>
2021-02-15 08:13:40 +0000

    class3

--- 030-graphs.Rmd
+++ 030-graphs.Rmd
@@ -9,6 +9,8 @@
 Videos from the class are in the PSY6422 [Google Drive](https://drive.google.com/drive/folders/1IbnGAO2Gn4u9T_qze5zhZyU9E8qNmdHh?usp=sharing) (UoS login required to access)
 
 <!-- Slides : [slides format](https://docs.google.com/presentation/d/1DbStojATmr9ZH4_23-F24uqZZ_9M9LkenGLFlwWZBig/edit?usp=sharing), [pdf format](). -->
+
+The live-coding demo used these files: [class3a.R](/static/class3a.R)
   
 ## Resources
 
@@ -24,12 +26,27 @@ Healy, K. (2018). [Data visualization: a practical introduction](https://socviz.
 We are going to be using the ggplot package for our visualisations:
 
 * Create Elegant Data Visualisations Using the Grammar of Graphics - [ggplot2](https://ggplot2.tidyverse.org/)
-
+* [Five Charts You’ve Never Used but Should](https://policyviz.com/2021/02/08/five-charts-youve-never-used-but-should/)
 ## Exercises
 
 Review the [introduction](https://socviz.co) and [Chapter 1 'Look At Data'](https://socviz.co/lookatdata.html) of Healy (2018)
 
+Work through [Chapter 2 'Get Started'](https://socviz.co/gettingstarted.html) from 2.3 onwards ("Things to know about R"), we are ignoring the material on RMarkdown for now (we'll get there, see [here](https://tomstafford.github.io/psy6422/rmarkdown.html).
+
 Work through [Chapter 3 'Make a plot'](https://socviz.co/makeplot.html)
 
 ## Checklist
-  
+
+Completed the exercises
+
+Understood the difference between variables and functions
+
+Understood that variables are typed (e.g. can be numeric or character)
+ * and how you detect what type a variable is
+
+In the console, understood
+  * the shortcut keys for the assignment operator <- 
+  * the use of tab for autocomplete
+  * the use up arrow to cycle through previous commands
+
+Understood indexing of vectors or data frames
---
+++ docs/static/class.3.R
@@ -0,0 +1,123 @@
+#We are following Healy (2018) from Section 2.3 https://socviz.co/gettingstarted.html
+
+## Things to know about R
+### Everything has a name
+### Everything is an object
+
+c(1, 2, 3, 1, 3, 5, 25)
+
+my_numbers <- c(1, 2, 3, 1, 3, 5, 25)
+
+your_numbers <- c(5, 31, 71, 1, 3, 21, 6)
+
+### You do things using functions
+
+# and by transforming data, saving the output
+
+my_numbers
+mean()
+
+
+mean(x = my_numbers)
+
+mean(x = your_numbers)
+
+mean(my_numbers)
+
+
+my_summary <- summary(my_numbers)
+
+my_summary
+
+
+### Functions come in libraries
+# and some are built in
+# showing off some transformation
+
+table(my_numbers)
+
+sd(my_numbers)
+
+my_numbers * 5
+
+my_numbers + 1
+
+my_numbers + my_numbers
+
+
+### If you're not sure what an object is, ask for its class
+
+class(my_numbers)
+class(my_summary)
+class(summary)
+
+my_new_vector <- c(my_numbers, "Apple")
+my_new_vector
+class(my_new_vector)
+
+#Types!
+
+a <- "1"
+b <- "2"
+
+a+b
+
+class(a)
+
+"dogs" > "cats"
+
+
+cats <- 10
+dogs <- 2
+
+cats > dogs
+
+#data frames!
+
+titanic
+
+#a note on case sensitivity
+Titanic
+
+#Healy uses a data frame he pre-created, so it is only "there" if you load the socviz library first
+#devtools::install_github("kjhealy/socviz") #you may need to install it first
+# note nonstandard install ie.. it is not install.packages("socviz")
+library(socviz)
+
+titanic
+class(titanic)
+
+titanic$percent
+
+#making a tibble, which is a tidyverse data frame
+titanic_tb <- as_tibble(titanic)
+titanic_tb
+
+
+
+### To see inside an object, ask for its structure, or use RStudio's object inspector
+
+str(my_numbers)
+str(my_summary)
+
+
+ggplot(data = titanic, aes(x = n, y = percent)) +
+    geom_point()
+
+## <----- PAIR EXERCISE --------->
+
+## Get data into R
+
+#1 first download this file by copying url to browser
+# https://cdn.rawgit.com/kjhealy/viz-organdata/master/organdonation.csv
+
+#2 #download and look at file using google sheets or excel'
+
+#3 assign the url to a variable (remember it is a string, so use "")
+
+#4 use that variable name to load the data from the internet
+# google "read_csv in r" for help
+
+#4b Bonus: load the same data from the file on your computer
+
+#5 now plot pop.dens against gdp using a scatter plot and tell me what you see
